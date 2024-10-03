4d2db307a7a0bcaf0922db95d15b583c76f1527c
dinnerdoggy <31261276+dinnerdoggy@users.noreply.github.com>
2024-10-03 15:22:11 -0500

    useState

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 'use client';
 
+import { useState } from 'react';
 import Page from '../api/jokeData';
 
 /* eslint-disable react/button-has-type */
 function Home() {
+  const [showComponent, setShowComponent] = useState(false);
   const handleClick = () => {
-    Page();
+    setShowComponent(!showComponent);
   };
   return (
     <div
@@ -19,6 +21,7 @@ function Home() {
     >
       Welcome to Next JS!
       <button onClick={handleClick}>Get a Joke</button>
+      {showComponent && <Page />}
     </div>
   );
 }
